3fc2cd8999aa20db1b93c1be1e468997b86a9b6b
barnabe <b.sellier27@hotmail.fr>
2022-05-16 14:37:48 +0200

    debut connexion

---
+++ controller.py
@@ -0,0 +1,8 @@
+import main
+
+def verifConnexion(login, password):
+    print(login, password)
+    #verif sql
+    if login == 'admin' and password == '123':
+        main.changeDisplay('entree')
+        print("connexion...")
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QLineEdit
 from PyQt5.QtWidgets import * 
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import * 
+import controller
+
+
 
 #QMainWindow
 class LoginDisplay(QWidget):
@@ -13,32 +16,43 @@ class LoginDisplay(QWidget):
         #self.setGeometry(300, 300, 350, 250)
         self.setWindowTitle("LPC")
 
-        loginInput = QLineEdit(self)
-        loginInput.setPlaceholderText("Identifiant")
-        loginInput.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.loginInput = QLineEdit(self)
+        self.loginInput.setPlaceholderText("Identifiant")
+        self.loginInput.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        passwordInput = QLineEdit(self)
-        passwordInput.setPlaceholderText("Mot de passe")
-        passwordInput.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.passwordInput = QLineEdit(self)
+        self.passwordInput.setPlaceholderText("Mot de passe")
+        self.passwordInput.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        self.connexionButton = QPushButton(self)
+        self.connexionButton.setText("Se connecter")
+
+        self.connexionButton.clicked.connect(self.connexion)
+
 
         layoutGrid = QGridLayout()
         self.setLayout(layoutGrid)
 
 
         layoutGrid.setColumnStretch(0, 1)
-        layoutGrid.setColumnStretch(3, 1)
+        layoutGrid.setColumnStretch(2, 1)
         layoutGrid.setRowStretch(0, 1)
-        layoutGrid.setRowStretch(4, 1)
+        layoutGrid.setRowStretch(5, 1)
 
-        layoutGrid.addWidget(loginInput, 2, 2 , 1, 1)
-        layoutGrid.addWidget(passwordInput, 3, 2, 1, 1)
-
-
+        layoutGrid.addWidget(self.loginInput, 2, 1 , 1, 1)
+        layoutGrid.addWidget(self.passwordInput, 3, 1, 1, 1)
+        layoutGrid.addWidget(self.connexionButton, 4, 1, 1, 1)
 
         self.showMaximized()
 
+
+    def connexion(self):
+        login = self.loginInput.text()
+        password = self.passwordInput.text()
+        controller.verifConnexion(login, password)
+
         
-class EntreeDisplay(QWidget):
+class EntreeDisplay(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -57,13 +71,28 @@ class EntreeDisplay(QWidget):
         self.setLayout(layoutGrid)
 
         self.showMaximized()
+    
 
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+
+
+
+        
+
+
+def changeDisplay(display):
+    if display == 'entree':
+        print("entree")
 
 
 def main():
     app = QApplication(sys.argv)
-    ex = EntreeDisplay()
+    ex = LoginDisplay()
     sys.exit(app.exec())
+    
+    
 
 
 if __name__ == '__main__':
