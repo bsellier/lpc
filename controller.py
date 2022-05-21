d8d36d03e8cf20e90c75d85cc27fc04df0ebedca
barnabe <b.sellier27@hotmail.fr>
2022-05-21 15:51:51 +0200

    fonctions sql

--- controller.py
+++ controller.py
@@ -1,9 +1,47 @@
 import main
+import identifiants
+import mysql.connector
+
+
+conn = mysql.connector.connect(host=identifiants.host, user=identifiants.user, password=identifiants.password,  database=identifiants.dbname)
+cur = conn.cursor()
+# def initDB():
+#     global
+    
+currentUser = ""
 
 def verifConnexion(login, password):
     print(login, password)
-    #verif sql
-    if login == 'admin' and password == '123':
-        #main.changeDisplay('entree', rootStack)
-        print("connexion...")
+
+    sql = "SELECT * from User WHERE login ='" + login  + "' and password ='" + password + "'"
+
+    cur.execute(sql)
+    raw = cur.fetchone()
+    if raw[0] == login and raw[1] == password:
+        currentUser = login
         return True
+  
+
+def ajouterUtilisateur(login, password, isAdmin):
+    sql = "SELECT * from User WHERE login ='" + login + "'"
+    cur.execute(sql)
+    myresult = cur.fetchall()
+
+    if(len(myresult) > 0):
+        print("utilisateur existe deja")
+    else:
+        sql = "INSERT INTO User values ('" + login + "','" + password + "'," + str(isAdmin) + ")"
+        cur.execute(sql)
+        conn.commit()
+
+def modifMDP(ancienMDP, nouvMDP):
+    if currentUser != "":
+        sql = "SELECT password from User WHERE User.login = '" + currentUser + "'"
+        cur.execute(sql)
+        if(cur.fetchone()[0] == ancienMDP):
+            sql = "UPDATE User SET password = '" + nouvMDP + "' WHERE User.login = '" + currentUser + "' AND password = '" + ancienMDP + "'"
+            cur.execute(sql)
+            conn.commit()
+        else:
+            print("mauvais mot de passe")
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
 import gui
+import controller
 
 if __name__ == '__main__':
+    #controller.currentUser ="user2"
+    #controller.modifMDP("abcd", "123")
     gui.initGUI()
